90eba375b9b19d165e062c03a234875105c88445
asahiiro <2703639341@qq.com>
2025-03-06 11:59:41 +0800

    dark mode done

--- mytodo/test.js
+++ mytodo/test.js
@@ -1,8 +1,11 @@
+
+//document.getElementById("themeButton").addEventListener("click", changeTheme());
+
 function changeTheme(){
     let theme = document.getElementById('theme');
     if(theme.getAttribute('href')=='light.css'){
-        theme.setAttribute('href') = 'dark.css';
+        theme.setAttribute('href','dark.css');
     } else {
-        theme.setAttribute('href') = 'light.css';
+        theme.setAttribute('href','light.css');
     }
 }
