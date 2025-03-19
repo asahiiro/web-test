d4776464a7b93c8395f9d0ace4c5d72afbb98a3d
asahiiro <2703639341@qq.com>
2025-03-19 23:28:58 +0800

    trail deleted

--- trial/button-menu/1.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('index-button').addEventListener('click', function() {
-    var indexContent = document.getElementById('index-content');
-    indexContent.classList.toggle('hidden');
-    if (indexContent.classList.contains('hidden')) {
-        this.textContent = 'Show Index';
-    } else {
-        this.textContent = 'Hide Index';
-    }
-});
