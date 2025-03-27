ac8ceedc4c89f59e65b42ba71a59d7f2b004224e
asahiiro <2703639341@qq.com>
2025-03-27 21:08:58 +0800

    btod

--- Bi2DeConvertor/script.js
+++ Bi2DeConvertor/script.js
@@ -1,5 +1,10 @@
 function convert () {
     const input = document.getElementById('number');
     const number = input.value;
-    number.forEach()
+    let decimal = 0;
+    number.split('').forEach((digit,index) => {
+        const power = number.length - 1 - index;
+        decimal += parseInt(digit)*Math.pow(2,power);
+    })
+    document.getElementById('result').textContent = decimal;
 }
